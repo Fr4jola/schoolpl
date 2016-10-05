94da51abe9f7b40d8f5710dfee98bf338a86c93c
Fr4jola <fr4jola_mdm@hotmail.com>
2016-10-05 18:02:11 -0300

    Initial commit

--- app/models/aluno.rb
+++ app/models/aluno.rb
@@ -1,2 +1,7 @@
 class Aluno < ApplicationRecord
+	validates :cpf, :rg, :data_nascimento, :nome, :phone, presence: true
+	validates :cpf, :rg, uniqueness: true
+	validates :cpf, length: { minimum: 11 }
+	validates :cpf, length: { maximum: 11 }
+	validates :cpf, numericality: true
 end
